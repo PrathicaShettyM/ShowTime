36959bab19215f9848916748a432593002092154
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2025-10-15 21:41:24 +0530

    Setup the server using express, mongoose and nodemon

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const env = require('dotenv');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+env.config();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/home', (req, res) => {
+    console.log(" API Endpoint");
+    return res.json({
+        success: true,
+        message: 'Fetched home'
+    });
+});
+
+app.listen(process.env.PORT, async () => {
+    console.log(`Server is running on the port ${process.env.PORT}`);
+    
+    // database connection
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Connected to the Database...");
+    } catch (error) {
+        console.log("Error while connecting to the Database: ", err);
+    }
+});
+
+
