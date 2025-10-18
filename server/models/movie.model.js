303bcce87014fc28a0611ed29ec0531c706f6478
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2025-10-18 06:25:49 +0530

    Added movie models

---
+++ server/models/movie.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+/*
+* Define the schema of the "movie resource" to be stored in the db
+*/
+
+const movieSchema = mongoose.model({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    casts: {
+        type: [String],
+        required: true
+    }, trailerUrl: {
+        type: String,
+        required: true
+    }, 
+    language: {
+        type: String,
+        required: true,
+        default: "English"
+    },
+    releaseDate: {
+        type: String,
+        required: true,
+    },
+    director: {
+        type: String,
+        required: true,
+        default: "RELEASED"
+    }
+},{
+    timestamps: true
+});
+
+const Movie = mongoose.model('Movie', movieSchema); // creates a new model
+module.exports = Movie;
